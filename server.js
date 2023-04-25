714b19da422a8e8dfc5da24cf1d5f4ef841d2e24
AZYxV <nschpro@gmail.com>
2023-04-25 14:08:06 +0200

    Etape 7 : put file OK

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require('path'); // Ajout de l'importation du module path
-const port = 3000;
+const port = 3100;
 const os = require('os');
 const fs = require('fs');
 const e = require("express");
+const bb = require('express-busboy');
+const {json} = require("express");
 
 const staticFilesPath = path.join(__dirname, 'front');
 
 app.use(express.static(staticFilesPath));
 
+app.use(express.json());
+
 const tmpDir = os.tmpdir();
+
+bb.extend(app, {
+    upload: true,
+    path: `${tmpDir}`,
+    allowedPath: /./
+});
 
 app.get('/api/drive', async (req, res) => {
     const getDir = await fs.promises.readdir(tmpDir, {
@@ -99,6 +109,135 @@ app.post('/api/drive', (req, res) => {
     });
 });
 
+app.post('/api/drive/:folder', (req, res) => {
+
+    const name = req.query.name;
+
+    const folder = req.params.folder;
+
+    if (!name)
+    {
+        return res.status(400).json({ error: 'Le paramètre "name" est requis' });
+    }
+
+    if (!/^[a-zA-Z0-9]+$/.test(name)) {
+        return res.status(400).json({ error: 'Le paramètre "name" ne doit contenir que des caractères alphanumériques' });
+    }
+
+    const folderDir = `${tmpDir}/${folder}`;
+
+    if (!fs.existsSync(folderDir)) {
+        return res.status(404).json({ error: 'Le dossier n\'existe pas' });
+    }
+
+    const newFolderDir = `${folderDir}/${name}`;
+
+    if (fs.existsSync(newFolderDir)) {
+        return res.status(409).json({ error: 'Le dossier existe déjà' });
+    }
+
+    fs.mkdir(newFolderDir, {recursive: true}, (err) => {
+        if (err)
+        {
+            return res.status(500).json({ error: 'Impossible de créer le dossier' });
+        }
+        res.status(201).json({ message: `Dossier créé avec succès : ${newFolderDir}` });
+    });
+});
+
+app.delete('/api/drive/:name', (req, res) => {
+
+    const name = req.params.name;
+
+    const folderDir = `${tmpDir}/${name}`;
+
+    if (!/^[a-zA-Z0-9]+$/.test(name)) {
+        return res.status(400).json({ error: 'Le paramètre "name" ne doit contenir que des caractères alphanumériques' });
+    }
+
+    if (!fs.existsSync(folderDir)) {
+        return res.status(404).json({ error: `${folderDir} n'existe pas` });
+    }
+
+    if (fs.lstatSync(folderDir).isDirectory()) { // Utiliser fs.lstatSync pour obtenir les informations de fichier
+        fs.rmdir(`${folderDir}`,{recursive: true}, (err) => {
+            if (err) {
+                return res.status(500).json({ error: `Impossible de supprimer le dossier ${folderDir}` });
+            }
+            res.status(200).json({ message: `Dossier ${folderDir} supprimé avec succès` });
+        });
+    } else {
+        fs.unlink(`${folderDir}`, (err) => {
+            if (err) {
+                return res.status(500).json({ error: `Impossible de supprimer le fichier ${folderDir}` });
+            }
+            res.status(200).json({ message: `Fichier ${folderDir} supprimé avec succès` });
+        });
+    }
+});
+
+app.delete('/api/drive/:folder/:name', (req, res) => {
+
+    const folder = req.params.folder;
+
+    const name = req.params.name;
+
+    const folderDir = `${tmpDir}/${folder}/${name}`;
+
+    if (!/^[a-zA-Z0-9]+$/.test(name)) {
+        return res.status(400).json({ error: 'Le paramètre "name" ne doit contenir que des caractères alphanumériques' });
+    }
+
+    if (!fs.existsSync(folderDir)) {
+        return res.status(404).json({ error: `${folderDir} n'existe pas` });
+    }
+
+    if (fs.lstatSync(folderDir).isDirectory()) { // Utiliser fs.lstatSync pour obtenir les informations de fichier
+        fs.rmdir(`${folderDir}`,{recursive: true}, (err) => {
+            if (err) {
+                return res.status(500).json({ error: `Impossible de supprimer le dossier ${folderDir}` });
+            }
+            res.status(200).json({ message: `Dossier ${folderDir} supprimé avec succès` });
+        });
+    } else {
+        fs.unlink(`${folderDir}`, (err) => {
+            if (err) {
+                return res.status(500).json({ error: `Impossible de supprimer le fichier ${folderDir}` });
+            }
+            res.status(200).json({ message: `Fichier ${folderDir} supprimé avec succès` });
+        });
+    }
+});
+
+app.put('/api/drive', (req, res) => {
+
+    const file = req.files.file;
+
+    const fileObj = JSON.parse(JSON.stringify(file));
+
+    const filePath = fileObj.file;
+
+    const fileName = fileObj.filename;
+
+    if (!file) {
+        return res.status(400).json({ error: 'Le fichier à upload est requis' });
+    }
+
+    if (fs.existsSync(`${tmpDir}/${fileName}`)) {
+        return res.status(409).json({ error: 'Le fichier existe déjà' });
+    }
+
+
+    fs.rename(`${filePath}`, `${tmpDir}/${fileName}`, (err) => {
+        if (err) {
+            console.error('Une erreur s\'est produite lors du déplacement du fichier :', err);
+            res.status(501).json(filePath);
+        } else {
+            console.log('Le fichier a été déplacé avec succès !');
+            res.status(200).json(filePath);
+        }
+    });
+});
 
 app.listen(port, () => {
     console.log(`Port: ${port}`);
